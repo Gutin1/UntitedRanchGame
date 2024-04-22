86e5eb1ef8d5b71bb4a3eabf381e5f107c3e2f20
Gutin1 <Claytonpinter@gmail.com>
2024-04-22 14:27:04 -0500

    todos

--- src/main/kotlin/dev/gutin/main/BlainsGame.kt
+++ src/main/kotlin/dev/gutin/main/BlainsGame.kt
@@ -5,6 +5,10 @@ import com.lehaine.littlekt.ContextListener
 import com.lehaine.littlekt.graphics.gl.ClearBufferMask
 
 class BlainsGame(context: Context) : ContextListener(context) {
+    // TODO heartbeat thread
+    // TODO load assets
+    // TODO
+
     override suspend fun Context.start() {
         // this is where we can use the context to add render calls, dispose, calls, etc. All the logic should go here.
 //        val texture = resourceVfs["texture.png"].readTexture() // reads a texture on the main thread from the resources
--- src/main/kotlin/dev/gutin/main/Main.kt
+++ src/main/kotlin/dev/gutin/main/Main.kt
@@ -3,11 +3,15 @@ package dev.gutin.main
 import com.lehaine.littlekt.createLittleKtApp
 
 fun main(args: Array<String>) {
+    // TODO load configuration
+    // TODO loading screen scene
+    // TODO enter into title screen
+
     val app = createLittleKtApp {
         width = 960
         height = 540
         vSync = true
-        title = "My First LittleKt App"
+        title = "Untitled Game"
     }
 
     app.start { BlainsGame(it) }
